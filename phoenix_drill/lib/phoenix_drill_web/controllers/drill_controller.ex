fc3248af77d03dcc17d666b918e4bc5093995423
boldPro <probold777@gmail.com>
2024-01-04 18:11:23 -0500

    navber router working

--- phoenix_drill/lib/phoenix_drill_web/controllers/drill_controller.ex
+++ phoenix_drill/lib/phoenix_drill_web/controllers/drill_controller.ex
@@ -12,7 +12,24 @@ defmodule PhoenixDrillWeb.RandomNumController do
   use PhoenixDrillWeb, :controller
 
   def random_number(conn, _params) do
-    render(conn, :random_num, random_num: Enum.random(1..100), layout: false)
+    render(conn, :random_num, random_num: Enum.random(1..100))
   end
 
+end
+
+defmodule PhoenixDrillWeb.NavBarController do
+  use PhoenixDrillWeb, :controller
+
+  def home(conn, _params)do
+    render(conn, :home, layout: false)
+  end
+
+    def about(conn, _params)do
+    render(conn, :about,  layout: false)
+  end
+
+    def projects(conn, _params)do
+
+    render(conn, :projects,  layout: false)
+  end
 end
---
+++ phoenix_drill/lib/phoenix_drill_web/controllers/nav_bar_html.ex
@@ -0,0 +1,5 @@
+defmodule PhoenixDrillWeb.NavBarHTML do
+  use PhoenixDrillWeb, :html
+
+  embed_templates "nav_bar_html/*"
+end
