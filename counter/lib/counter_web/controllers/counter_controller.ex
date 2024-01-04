c6a464d561694c98c63a026284d92b2ca26c4690
boldPro <probold777@gmail.com>
2024-01-04 14:22:56 -0500

    Phoenix counter modif

--- counter/lib/counter_web/controllers/counter_controller.ex
+++ counter/lib/counter_web/controllers/counter_controller.ex
@@ -5,13 +5,18 @@ defmodule CounterWeb.CounterController do
     # The home page is often custom made,
     # so skip the default app layout.
     IO.inspect(params, label: "PARAMS")
-    render(conn, :count, count: String.to_integer(params["count"]) || 0)
+
+    render(conn, :count,
+     count: if params["count"] do
+       String.to_integer(params["count"])
+       else 0
+      end)
   end
 
   def increment(conn, params) do
-  current_count = String.to_integer(params["count"])
-  increment_by = String.to_integer(params["increment_by"])
-  IO.inspect(params, label: "INC PARS")
-  render(conn, :count, count: current_count + increment_by)
+    current_count = String.to_integer(params["count"])
+    increment_by = String.to_integer(params["increment_by"])
+    IO.inspect(params, label: "INC PARS")
+    render(conn, :count, count: current_count + increment_by)
 end
 end
